8241011a1b7c8383805cc66397f612c3bdb69238
akkas_dev <codersadhin@gmail.com>
2024-06-17 01:27:40 +0800

    sheet_1 F

---
+++ Sheet_1_(Data_type_Conditions)/F_Digits_Summation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long a,b;
+    cin >> a >> b;
+
+    int ans=(a%10)+(b%10);
+
+    cout << ans << endl;
+
+    return 0;
+}
