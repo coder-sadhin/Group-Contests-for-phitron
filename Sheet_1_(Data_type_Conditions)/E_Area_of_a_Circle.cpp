c409a66ac46ef1f04ebefe3cfbd5727be6f189d5
akkas_dev <codersadhin@gmail.com>
2024-06-17 01:10:02 +0800

    sheet_1 E

---
+++ Sheet_1_(Data_type_Conditions)/E_Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float X;
+    cin >> X;
+
+    double mul = 3.141592653 * X * X;
+
+    cout <<fixed<<setprecision(9)<< mul << endl;
+
+    return 0;
+}
