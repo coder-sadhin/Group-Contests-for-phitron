e8ca5f1395d90935d0dddc98f795af771bf0387b
akkas_dev <codersadhin@gmail.com>
2024-06-17 00:17:17 +0800

    sheet_1 data type

---
+++ Sheet_1_(Data_type_Conditions)/B_Basic_Data_Types.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i; long long li; char c; float fn; double dn;
+    cin>>i>>li>>c>>fn>>dn;
+    cout<<i<<endl<<li<<endl<<c<<endl<<fixed<<setprecision(2)<<fn<<endl<<setprecision(1)<<dn<<endl;
+    
+    return 0;
+}
