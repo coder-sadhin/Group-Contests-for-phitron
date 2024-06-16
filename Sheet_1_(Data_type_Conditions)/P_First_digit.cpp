ab07e875e1412205a67d9f8c9e9ac13a8ac5e193
akkas_dev <codersadhin@gmail.com>
2024-06-17 02:44:15 +0800

    sheet_1 P

---
+++ Sheet_1_(Data_type_Conditions)/P_First_digit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin>>A;
+    int num=A/1000;
+    if(num%2==0){
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+    return 0;
+ 
+}
