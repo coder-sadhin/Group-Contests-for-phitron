9824c38d4db6cd68a54dcb0978b32c8091405dae
akkas_dev <codersadhin@gmail.com>
2024-06-19 01:04:51 +0800

    sheet_2 N

---
+++ Sheet_2_(Loops)/N_Numbers_Histogram.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char S;
+    int N;
+    cin >> S >> N;
+    
+    while (N--)
+    {
+        int x;
+        cin>>x;
+        for (int i = 0; i < x; i++)
+        {
+            cout<<S;
+        }
+        cout<<endl;
+    }
+    
+    
+    return 0;
+}
